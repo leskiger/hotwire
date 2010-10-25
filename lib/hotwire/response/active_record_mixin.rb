68259f458185d68084c7d3c69566ce1b2cd3ad86
Les Kiger <les@sunquake.net>
2010-10-25 13:29:04 -0700

    Added set_data for AR collections

--- lib/hotwire/response/active_record_mixin.rb
+++ lib/hotwire/response/active_record_mixin.rb
@@ -5,7 +5,7 @@ module Hotwire
     module ActiveRecordMixin
       def add_columns_with_active_record_model(args)
         if args.respond_to?(:ancestors) and args.ancestors.include? ActiveRecord::Base
-          args = args.columns.map do |col| 
+          args = args.columns.sort_by{|c| c.name }.map do |col| 
             [normalize_active_record_type(col.type), {:id => col.name, :label => col.name }]
           end
         end
@@ -13,11 +13,22 @@ module Hotwire
         
       end
       
+      def set_data_with_active_record_collection(data)
+        data = rows_from_active_record_collection(data) if data.first.is_a?(ActiveRecord::Base)
+        set_data_without_active_record_collection(data)
+      end
+      
       def self.included(base)
         base.send :alias_method_chain, :add_columns, :active_record_model
+        base.send :alias_method_chain, :set_data, :active_record_collection
       end
       
     private
+      
+      # Converts an Array of ActiveRecord objects to an Array of data points based on columns setup for the response.
+      def rows_from_active_record_collection(collection)
+        collection.map { |obj| self.columns.map { |c| obj[c[:id]] } }
+      end
       
       # Convert an ActiveRecord type string to Wire Protocol types
       def normalize_active_record_type(type)
--- test/response/test_active_record_mixin.rb
+++ test/response/test_active_record_mixin.rb
@@ -8,20 +8,39 @@ class TestActiveRecordMixin < Test::Unit::TestCase
     context "created from a valid request" do
       setup { @response = Hotwire::Response::Base.new(valid_request) }
       
-      context "add_columns" do
+      context "add_columns with a Model" do
         setup { @response.add_columns(Person) }
         
         should "add columns to match the ActiveRecord object" do
-          expected = [{:type => "number", :id => "id", :label => 'id'},
-                      {:type => "string", :id => "name", :label => 'name'},
-                      {:type => "string", :id => "bio", :label => 'bio'},
-                      {:type => "number", :id => "age", :label => 'age'},
-                      {:type => "number", :id => "weight", :label => 'weight'},
-                      {:type => "datetime", :id => "birth_date", :label => 'birth_date'},
-                      {:type => "boolean", :id => "tall", :label => 'tall'}]
+          expected = [{:type=>"number", :label=>"age", :id=>"age"},
+                      {:type=>"string", :label=>"bio", :id=>"bio"},
+                      {:type=>"datetime", :label=>"birth_date", :id=>"birth_date"},
+                      {:type=>"number", :label=>"id", :id=>"id"},
+                      {:type=>"string", :label=>"name", :id=>"name"},
+                      {:type=>"boolean", :label=>"tall", :id=>"tall"},
+                      {:type=>"number", :label=>"weight", :id=>"weight"}]
                       
           assert_equal expected, @response.columns
         end
+      end
+      
+      context "set_data with a collection" do
+        setup do
+          Person.destroy_all
+          @time = Time.utc(1980, 9, 15, 12, 22, 23)
+          @bob = Person.create(:name => "Bob", :age => 30, :bio => "A Nice Guy", :birth_date => 30.years.ago, :tall => true, :weight => 155.5)
+          @fred = Person.create(:name => "Fred", :age => 22, :bio => "A Nice Guy Too", :birth_date => 22.years.ago, :tall => false, :weight => 110.5)
+          @response.add_columns(Person)
+          @response.set_data(Person.all)
+        end
+        
+        should "add rows from the collection" do
+          expected = [[30, "A Nice Guy", @bob.birth_date, 1, "Bob", true, @bob.weight],
+                      [22, "A Nice Guy Too", @fred.birth_date, 2, "Fred", false, @fred.weight]]
+                      
+          assert_equal expected.to_s, @response.data.to_s
+        end
+        
       end
 
     end
